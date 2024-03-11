394aa3b25c52020198f74e92c51ff51f9679f7cd
priyaram8 <129445016+priyaram8@users.noreply.github.com>
2024-03-11 16:45:34 -0700

    Add files via upload

---
+++ auth0Provider.js
@@ -0,0 +1,19 @@
+// src/index.js
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+    <Auth0Provider
+        domain="dev-ndj5x6rtujc8xapi.us.auth0.com"
+        clientId="nnH5m9WRacJOUh4F5F0MT4sxWttasIq2"
+        authorizationParams={{
+            redirect_uri: window.location.origin,
+        }}
+    >
+        <App />
+    </Auth0Provider>
+);
